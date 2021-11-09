3ab883c2d5fbf853cd2ca7a3a060a4528cf6ee72
Nguyễn Quan Huy <64772864+nqhuy2509@users.noreply.github.com>
2021-11-09 22:15:00 +0700

    add mesh function

--- main.js
+++ main.js
@@ -1,50 +1,75 @@
 // import * as THREE from './assets/lib/three'
 
-function init(){
-    var scene = new THREE.Scene()
-    var gui = new dat.GUI();
-    gui.domElement.id = 'gui';
-    var renderer = new THREE.WebGLRenderer()
-    var camera = new THREE.PerspectiveCamera(
-        45,
-        window.innerWidth/window.innerHeight,
-        1,
-        1000
-    );
-    var grid = new THREE.GridHelper(100, 100);
-    scene.add(grid);
 
-    camera.position.x = 10;
-    camera.position.y = 5;
-    camera.position.z = 10;
-    
-    function updateCamera(){
-        camera.updateProjectionMatrix();
-    }
+var scene = new THREE.Scene()
+var gui = new dat.GUI();
+gui.domElement.id = 'gui';
+var renderer = new THREE.WebGLRenderer()
+var camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth/window.innerHeight,
+    1,
+    1000
+);
+var grid = new THREE.GridHelper(100, 100);
+scene.add(grid);
 
-    var camGUI = gui.addFolder('Camera');
-    camGUI.add(camera,'fov',0,175).name('FOV').onChange(updateCamera);
-    camGUI.add(camera,'near',0.1,50,0.1).name('near').onChange(updateCamera);
-    camGUI.add(camera,'far',1000,5000,10).name('far').onChange(updateCamera);
+camera.position.x = 10;
+camera.position.y = 5;
+camera.position.z = 10;
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor('rgb(75,75,75)');
-    document.getElementById('webgl').appendChild(renderer.domElement);
-    var controls = new THREE.OrbitControls(camera,renderer.domElement);
-
-    update(renderer,scene,camera,controls);
-    renderer.render(scene,camera);
+function updateCamera(){
+    camera.updateProjectionMatrix();
 }
+
+
 
 function addMesh(number){
-    switch (number) {
-        case 1:
-            getBox(1,1,1)
-            break;
-        default:
-            break;
+    var geometry;
+    
+    var material = new THREE.MeshBasicMaterial(
+        {color: 0xffffff}
+    );
+
+    if (number ==1){
+        geometry =  new THREE.BoxGeometry(7,7,7);
+          
     }
+
+    if (number ==2){
+        geometry = new THREE.SphereGeometry(5,100,100);
+    }
+
+    if (number ==3){
+        geometry = new THREE.ConeGeometry(4,7,100);
+    }
+
+    if (number ==4){
+        geometry =  new THREE.CylinderGeometry(4,4,7,100);
+    }
+
+    if (number==5){
+        geometry = new THREE.TorusGeometry(3, 1, 30, 100);
+    }
+
+    var mesh = new THREE.Mesh(geometry,material);
+    scene.remove(scene.children[1])
+    scene.add(mesh);
 }
+
+var camGUI = gui.addFolder('Camera');
+camGUI.add(camera,'fov',0,175).name('FOV').onChange(updateCamera);
+camGUI.add(camera,'near',0.1,50,0.1).name('near').onChange(updateCamera);
+camGUI.add(camera,'far',1000,5000,10).name('far').onChange(updateCamera);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor('rgb(75,75,75)');
+
+document.getElementById('webgl').appendChild(renderer.domElement);
+var controls = new THREE.OrbitControls(camera,renderer.domElement);
+
+update(renderer,scene,camera,controls);
+renderer.render(scene,camera);
+
 
 
 function update(renderer, scene, camera, controls){
@@ -55,18 +80,5 @@ function update(renderer, scene, camera, controls){
     })
 }
 
-function getBox (w,h,d){
-    var geometry = new THREE.BoxGeometry(w,h,d);
-    var material = new THREE.MeshBasicMaterial({
-        color: 0x0000ff
-    });
-    var mesh = new THREE.Mesh(
-        geometry,
-        material
-    );
-    return mesh
-}
-
-init()
 
 
